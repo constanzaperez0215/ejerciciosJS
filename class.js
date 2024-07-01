a20a9498d914b3aeb50def7e0f038a8ea0ab93d7
Constanza Pérez <constanza.vp.o@gmail.com>
2024-07-01 15:55:47 -0400

    Clases // orientado a objetos

---
+++ class.js
@@ -0,0 +1,13 @@
+class zoologico{
+    constructor(animal, edad, peso, ubicacion) {
+        this.animal = animal
+        this.edad = edad
+        this.peso = peso
+        this.ubicacion = ubicacion
+    }
+}
+
+const zoologico1 = new zoologico ('Elefante', 15, 5200, 'África')
+const zoologico2 = new zoologico ('Jirafa', 7, 1500, 'África')
+const zoologico3 = new zoologico ('Tigre de bengala', 3, 180, 'India')
+const zoologico4 = new zoologico ('Zorro Artico', 5, 8, 'Círculo polar Ártico')
